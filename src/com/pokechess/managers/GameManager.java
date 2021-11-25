b351b9059f418f095b46627365c2d266fe979491
ryuddaeng <valdez.charisse123@gmail.com>
2021-11-25 20:51:14 +0800

    initial commit

--- src/com/pokechess/managers/GameManager.java
+++ src/com/pokechess/managers/GameManager.java
@@ -1,32 +1,61 @@
 package com.pokechess.managers;
 
-
-import java.util.Objects;
 import java.util.Scanner;
 
 public class GameManager {
-    Scanner scn = new Scanner(System.in);
-    private boolean loop = true;
+    private PokemonSelectManager pokemonSelectManager;
 
-    PokemonSelectManager pokemonSelectManager = new PokemonSelectManager();
+    public GameManager(){
+        this.pokemonSelectManager = new PokemonSelectManager();
 
-    public void startGame(){
-        pokemonSelectManager.askName();
-        pokemonSelectManager.showPokemonSelect();
+    }
+    /*
+        Title screen input loop (will reprint title screen and ask for a valid user input)
+    */
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do{
+            this.displayScreen();
+            input = scanner.nextLine();
+            input = input.toUpperCase();
+        } while(!this.isValid(input));
+        this.processInput(input);
+        // this.pokemonSelectManager.run();
     }
 
-    public void showTitleScreen(){
-        System.out.println("POKECHESS UNITE");
-        do {
-            System.out.print("Type \"START\" to begin: ");
-            String input = scn.nextLine();
-            if (input.equalsIgnoreCase("START"))
-                loop = false;
-            else if (input.equalsIgnoreCase("EXIT"))
+    /*
+       Returns true if the input contains "START" or "EXIT"
+       and returns false if not
+    */
+    private boolean isValid(String input){
+
+        switch(input) {
+            case "START":
+            case "EXIT GAME":
+                return true;
+            default:
+                return false;
+        }
+    }
+    /*
+        Processes input and launches pokemon select screen
+     */
+    private void processInput(String input){
+
+        switch(input) {
+            case "START":
+                this.pokemonSelectManager.run();
+                break;
+            case "EXIT GAME":
                 System.exit(0);
-            else
-                System.out.println("ERROR: Game does not recognize input. Try again.");
-        } while (loop);
-        startGame();
+                break;
+        }
     }
+    private void displayScreen(){
+        System.out.println("\n\n\n POKECHESS GAME \n\n\n");
+        System.out.println("\n\n\n [USER INPUT]:  ");
+
+    }
+
 }
