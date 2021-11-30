d98f829ff4b9af89a6b2bb3bcf184bbcfb040329
Eugene Sillona <93899090+jojiSillona@users.noreply.github.com>
2021-11-30 21:37:39 +0800

    Revert "Pokemon Selection (November 30, 2021)"

--- src/com/pokechess/player/Player.java
+++ src/com/pokechess/player/Player.java
@@ -11,12 +11,6 @@ public class Player {
     private boolean loop = true;
 
     Scanner scn = new Scanner(System.in);
-
-    public Player(){
-        for(int i = 0; i < pokemonTeam.length; i++){
-            pokemonTeam[i] = new Pokemon("non", 0 ,0, 0, 0, 0, 0);
-        }
-    }
 /*
     public void commandPokemon(){
         for(moves = 0; moves < 3; moves++){
@@ -42,10 +36,6 @@ public class Player {
             return "";
         else
             return pokemonTeam[index].getBattleType();
-    }
-
-    public String getName(int index){
-        return pokemonTeam[index].getName();
     }
 
     public void setName(String name){
